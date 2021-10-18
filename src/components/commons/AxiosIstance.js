eeae8cf79950fb1e7c70ed63952ff051caddb6cf
FrancescoLuzzi <francescoluzzi99@libero.it>
2021-10-18 18:11:35 +0200

    change Domain

--- src/components/commons/AxiosIstance.js
+++ src/components/commons/AxiosIstance.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const baseURL= 'http://luzzipi.luzzidomain/scontriniDB/api/';
 
-export const AxiosIstance=axios.create({
-    baseURL: baseURL
-  });
+export const AxiosIstance = axios.create({
+	baseURL: baseURL,
+});
