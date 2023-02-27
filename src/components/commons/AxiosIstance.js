5daa9b407430465b04934f7773011ed3ec5caa20
FrancescoLuzzi <francescoluzzi99@libero.it>
2023-02-27 19:52:11 +0100

    added .gitattributes

--- src/components/commons/AxiosIstance.js
+++ src/components/commons/AxiosIstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL= 'http://luzzipi.luzzidomain/scontriniDB/api/';
+const baseURL = 'http://luzzipi.luzzidomain/scontriniDB/api/';
 
 export const AxiosIstance = axios.create({
 	baseURL: baseURL,
